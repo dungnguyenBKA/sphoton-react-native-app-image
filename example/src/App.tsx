8c20164a82bbb743a3a43eef66a86ce53c350f16
dungnguyenBKA <dungdasua365@gmail.com>
2022-02-13 02:36:09 +0700

    test5

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,20 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import AppImage from 'react-native-sphoton-app-image';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <AppImage/>
+      <AppImage
+        source={{
+          uri: 'https://images.contentstack.io/v3/assets/blt187521ff0727be24/blt41c476486b063ef8/60ee13df31f9ee2ab08a4dfe/Yasuo_0.jpg'
+        }}
+        style={{
+          height: 100,
+          aspectRatio: 1,
+        }}
+      />
     </View>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { Text } from 'react-native';
+import dayjs from 'dayjs';
+// import FastImage, { FastImageProps } from 'react-native-fast-image';
 
-const AppImage: React.FC = () => {
-  return <Text>Test npm package</Text>
+interface AppImageProps {
+
 }
 
-export default AppImage
+const AppImage: React.FC<AppImageProps> = ({  }) => {
+  const time = dayjs('2022-02-13').toString()
+  return <Text>Hi, {time}</Text>;
+};
+
+export default AppImage;
